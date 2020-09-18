585f1ba32ae614ef567c7e03f53d82bbcc313269
Adam Morley <adam.morley@gmail.com>
2020-09-18 15:40:22 -0700

    add memoization example

---
+++ reLearn/memoization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from functools import lru_cache
+
+@lru_cache(maxsize=256)
+def fib(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    return fib(num-1) + fib(num-2)
+
+print(fib(35))
+print(fib(42))
